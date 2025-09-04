744dcf764b0c121620593284fa98b46a827d08ac
Ignacio marquez <66044641+ignacioMrkz@users.noreply.github.com>
2025-09-05 00:47:34 +0200

    Add scroll to top on route change

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useScrollToTop } from './hooks/useScrollToTop';
 import Navigation from './components/Navigation';
 import HomePage from './pages/HomePage';
 import ContributorsPage from './pages/ContributorsPage';
 import PressPage from './pages/PressPage';
 import ActivatePage from './pages/ActivatePage';
 
+const AppContent: React.FC = () => {
+  useScrollToTop();
+
+  return (
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/contributors" element={<ContributorsPage />} />
+        <Route path="/prensa" element={<PressPage />} />
+        <Route path="/activa-tu-pueblo" element={<ActivatePage />} />
+      </Routes>
+    </>
+  );
+};
+
 function App() {
   return (
     <Router>
       <div className="min-h-screen">
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/contributors" element={<ContributorsPage />} />
-          <Route path="/prensa" element={<PressPage />} />
-          <Route path="/activa-tu-pueblo" element={<ActivatePage />} />
-        </Routes>
+        <AppContent />
       </div>
     </Router>
   );
