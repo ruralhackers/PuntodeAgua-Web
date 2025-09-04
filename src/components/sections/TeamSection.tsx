d3a261e97cb2ce2b7cfd6ea2ef10753e55283ea1
Ignacio marquez <66044641+ignacioMrkz@users.noreply.github.com>
2025-09-04 22:31:17 +0200

    Update Card 5 to use foto1.png

--- src/components/sections/TeamSection.tsx
+++ src/components/sections/TeamSection.tsx
@@ -96,8 +96,8 @@ const TeamSection: React.FC = () => {
           >
             <div className="absolute top-2 left-2 bg-black/50 text-white text-xs px-2 py-1 rounded z-30">5</div>
             <img 
-              src="/HackerDays_grupo_2.JPG" 
-              alt="Hacker Days group photo" 
+              src="/foto1.png" 
+              alt="Foto 1" 
               className="w-full h-full object-cover"
             />
           </motion.div>
